4c36c75cad43910e0fbea879a45f859e8d242dfb
DotCla <91472177+Dotcla@users.noreply.github.com>
2025-04-10 17:40:27 +0200

    path fixato

--- src/main/java/com/theknife/Main.java
+++ src/main/java/com/theknife/Main.java
@@ -18,8 +18,8 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        // Carica il file usando un path relativo
-        String filePath = System.getProperty("user.dir")+"\\src\\main\\resources\\dataset_sanificato_2.csv"; // o "data/file.txt" se il file si trova in una sottodirectory
+         //Carica il file usando un path relativo
+        String filePath = System.getProperty("user.dir")+"/src/main/resources/Dataset/datafiles/dataset_sanificato_2.csv"; // o "data/file.txt" se il file si trova in una sottodirectory
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = reader.readLine()) != null) {
@@ -27,7 +27,7 @@ public class Main {
             }
         } catch (IOException e) {
             System.out.println("File non trovato.");
-            //System.out.println();
         }
+        //System.out.println(System.getProperty("user.dir")+"/src/main/resources/Dataset/datafiles/dataset_sanificato_2.csv");
     }
 }
