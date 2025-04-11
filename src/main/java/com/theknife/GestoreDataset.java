adfbe6c8ced504494d9ce365a7ed63b1a82a97d0
ClaudioerB <164493700+ClaudioerB@users.noreply.github.com>
2025-04-11 18:29:02 +0200

    si fossi foco

---
+++ src/main/java/com/theknife/GestoreDataset.java
@@ -0,0 +1,25 @@
+package com.theknife;
+
+
+
+public class GestoreDataset {
+    public GestoreDataset() {
+        
+    }
+
+    private int numeroRighe() {
+        
+        return dati.length;
+    }
+    public String[] ricercaGenerale(String input, String dati[][]) {
+        String[] risultati = new String[20];
+        for (int i = 0; i < dati.length; i++) {
+            for (int j = 0; j < dati[i].length; j++) {
+                if (dati[i][j].toLowerCase().contains(input.toLowerCase())) {
+                    System.out.println("Trovato: " + dati[i][j]);
+                }
+            }
+        }
+        return risultati;
+    }
+}
