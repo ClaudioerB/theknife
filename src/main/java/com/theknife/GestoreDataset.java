15823612218c4fc369decbff08800231bbd353ff
ClaudioerB <164493700+ClaudioerB@users.noreply.github.com>
2025-04-11 18:55:43 +0200

    contaRighe

--- src/main/java/com/theknife/GestoreDataset.java
+++ src/main/java/com/theknife/GestoreDataset.java
@@ -1,15 +1,30 @@
 package com.theknife;
 
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class GestoreDataset {
+    public String[][]dataSet;
     public GestoreDataset() {
-        
+        dataSet=new String[numeroRighe()][13];
     }
 
     private int numeroRighe() {
-
-        return 1;
+        String HOME = System.getProperty("user.dir");
+        String filePath = HOME+"/src/main/resources/Dataset/datafiles/dataset_ristoranti.csv";
+        int righe = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            
+            
+            while ((reader.readLine()) != null) {
+                righe++;
+            }
+            
+        } catch (IOException e) {
+            System.out.println("File non trovato.");
+        }
+        return righe;
     }
     public String[] ricercaGenerale(String input, String dati[][]) {
         String[] risultati = new String[20];
