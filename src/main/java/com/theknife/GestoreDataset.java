7629cd0056382703c7a25cdc7463c0a3200b4b74
ClaudioerB <164493700+ClaudioerB@users.noreply.github.com>
2025-04-15 14:06:36 +0200

    GestoreDataset update

--- src/main/java/com/theknife/GestoreDataset.java
+++ src/main/java/com/theknife/GestoreDataset.java
@@ -5,14 +5,54 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class GestoreDataset {
-    public String[][]dataSet;
+    private String[][] dataSet;
+    private String filePath;
+    
     public GestoreDataset() {
+        filePath = System.getProperty("user.dir")+"/src/main/resources/Dataset/datafiles/dataset_ristoranti.csv"; // o "data/file.txt" se il file si trova in una sottodirectory
         dataSet=new String[numeroRighe()][13];
+        inserimentoDati();
+        System.out.println(dataSet[0][0]);
+        System.out.println(dataSet[0][1]);
+
     }
 
+    public String[][] getDataSet() {
+        return dataSet;
+    }
+
+    public void setDataSet(String[][] dataSet) {
+        this.dataSet = dataSet;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    private void inserimentoDati() {
+        String[] appoggio;
+        int iRow = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                appoggio = line.split(";");
+                for(int i = 0; i<13;i++){
+                    dataSet[iRow][i] = appoggio[i];
+                }
+                iRow++;
+            }
+        } catch (IOException e) {
+            System.out.println("File non trovato.");
+        }
+    }
+    
     private int numeroRighe() {
-        String HOME = System.getProperty("user.dir");
-        String filePath = HOME+"/src/main/resources/Dataset/datafiles/dataset_ristoranti.csv";
+        
+        
         int righe = 0;
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             
--- src/main/java/com/theknife/Main.java
+++ src/main/java/com/theknife/Main.java
@@ -19,23 +19,9 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        String[][] mat = new String[17738][13];
-        String filePath = HOME+"/src/main/resources/Dataset/datafiles/dataset_ristoranti.csv"; // o "data/file.txt" se il file si trova in una sottodirectory
-        String[] appoggio;
-        int iRow = 0;
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                appoggio = line.split(";");
-                for(int i = 0; i<13;i++){
-                    mat[iRow][i] = appoggio[i];
-                }
-                iRow++;
-            }
-            SOP(mat[2][0]);
-        } catch (IOException e) {
-            SOP("File non trovato.");
-        }
+        GestoreDataset gestore = new GestoreDataset();
+        String[][] dataSet = gestore.getDataSet();
+        
         //System.out.println(System.getProperty("user.dir")+"/src/main/resources/Dataset/datafiles/dataset_sanificato_2.csv");
     }
 }
