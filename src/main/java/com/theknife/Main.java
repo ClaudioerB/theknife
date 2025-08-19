ea5addfcf50f8382093927827a29f7643fa81e57
DotCla <91472177+Dotcla@users.noreply.github.com>
2025-08-19 15:07:51 +0200

    Import grafica

--- src/main/java/com/theknife/Main.java
+++ src/main/java/com/theknife/Main.java
@@ -16,7 +16,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Gestore gestore =new Gestore();
+        //Gestore gestore =new Gestore();
         Grafica.main(args);
 
         //test(gestore);
