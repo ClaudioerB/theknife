441834aa6a750f56a801d18e5fb513724bd6ec73
ClaudioerB <claudiobonci9@gmail.com>
2026-01-23 13:41:26 +0100

    Update grafica crea ristorante

--- src/main/java/com/mycompany/theknife/ControllerCreaRistorante.java
+++ src/main/java/com/mycompany/theknife/ControllerCreaRistorante.java
@@ -1,5 +1,68 @@
 package com.mycompany.theknife;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 public class ControllerCreaRistorante {
+    @FXML
+    private javafx.scene.control.TextField nomeRistoranteField;
+    @FXML
+    private TextField indirizzoRistoranteField;
+    @FXML
+    private TextField telefonoRistoranteField;
+    @FXML
+    private ListView tipoCucinaRistoranteListView;
+    @FXML
+    private TextField sitoWebRistoranteField;
+    @FXML
+    private TextField cittaRistoranteField;
+    @FXML
+    private TextField statoRistoranteField;
+    @FXML
+    private TextField awardField;
+    @FXML
+    private MenuButton prezzoMenuButton;
+    @FXML
+    private CheckMenuItem prezzo1CheckMenuItem;
+    @FXML
+    private CheckMenuItem prezzo2CheckMenuItem;
+    @FXML
+    private CheckMenuItem prezzo3CheckMenuItem;
+    @FXML
+    private CheckMenuItem prezzo4CheckMenuItem;
+    @FXML
+    private TextArea descrizioneRistoranteTextArea;
+    @FXML
+    private ListView recensioniRistoranteListView;
+    @FXML
+    private ListView serviziRistoranteListView;
+    @FXML
+    private CheckBox prenotazioniCheckBox;
+    @FXML
+    private CheckBox consegnaCheckBox;
+    @FXML
+    private ImageView theKnifeImageView;
+    @FXML
+    private Button modificaTipiCucinaButton;
+    @FXML
+    private Button modificaServiziButton;
     
+    public ControllerCreaRistorante(){
+
+    }
+
+    public void initialize() {
+        
+    }
+
+
 }
